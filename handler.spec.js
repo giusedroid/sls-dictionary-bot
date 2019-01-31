fb199a6273431c27dd3d00832fea8707a0827102
Giuseppe Battista <giuseppebattista@giuseppistasmbp.home>
2019-01-31 20:00:06 +0000

    [EDIT] making tests more meaningful

--- handler.spec.js
+++ handler.spec.js
@@ -17,18 +17,18 @@ describe("The handler function", () => {
 describe("The S3 to DynamoDB Pipeline", function(){
     const bucket = require("./lib/bucket.model")({}, "");
     const mockActions = R.identity;
-    const mockEvent = require("./assets/s3Event");
-    const mockDownload = () => readFile("./assets/help.json");
+    const mockEvent = require("./assets/s3Event.test");
+    const mockDownload = key => readFile(`./${key}`);
     const mockBucket = {
         itemsFromEvent: bucket.itemsFromEvent,
         decode: bucket.decode,
         download: mockDownload
     };
     
-    it("executes", done => {
+    it("executes correctly", done => {
         const update = handler.testUpdateF(mockActions, mockBucket, R.identity );
         update(mockEvent)
-            .then( res => {expect(res.length).to.equal(8);})
+            .then( res => {console.log(res); expect(res.length).to.equal(7);})
             .then( done );
     });
 });
