a5559b339b6159a68a0f217ae91107d7acf11c2b
Giuseppe Battista <giuseppebattista@Giuseppe-Battistas-MacBook-Pro.local>
2019-02-05 21:57:09 +0000

    [EDIT] case sensitiveness...

--- handler.spec.js
+++ handler.spec.js
@@ -117,7 +117,7 @@ describe("The S3 to DynamoDB Pipeline", function(){
         const { itemsFromEvent } = require("./lib/bucket.model")({}, "");
     
         const mockActions = R.identity;
-        const mockEvent = require("./assets/s3Event.test");
+        const mockEvent = require("./assets/s3event.test");
         const mockDownload = key => readFile(`./${key}`);
         const decode = buffer => JSON.parse(buffer.toString("utf-8"));
         
