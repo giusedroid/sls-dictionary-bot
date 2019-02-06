bf10b37a609f4dc67036c2b67fd86fe035bda730
Giuseppe Battista <giuseppebattista@Giuseppe-Battistas-MacBook-Pro.local>
2019-02-06 18:10:07 +0000

    [ci skip] documenting the handlers

--- handler.js
+++ handler.js
@@ -8,25 +8,31 @@ const TABLE  = process.env.TABLE_NAME;
 const BUCKET = process.env.BUCKET_NAME;
 const TOKEN  = process.env.BOT_TOKEN;
 const CHALLENGE_TOKEN = process.env.CHALLENGE_TOKEN;
-const REGION = process.env.AWS_REGION || "eu-west-1";
+const REGION = R.defaultTo("eu-west-1", process.env.AWS_REGION);
+
 AWS.config.update({ region: REGION });
 
 // dynamo :: DynamoDB Client
-const dynamo = new AWS.DynamoDB({ apiVersion: "2012-10-08"});
-// term :: Term Model
+const dynamo = new AWS.DynamoDB({ apiVersion: "2012-10-08" });
+// term :: Model Term
 const term = require("./lib/term.model")(dynamo, TABLE);
 
 // s3 :: S3 Client
-const s3 = new AWS.S3({ apiVersion: "2006-03-01"});
-// bucket :: Bucket Model
+const s3 = new AWS.S3({ apiVersion: "2006-03-01" });
+// bucket :: Model Bucket
 const bucket = require("./lib/bucket.model")(s3, BUCKET);
 
 // bot :: Slack Bot
 const bot = new Slack( {token: TOKEN});
 
-// dictionaryF :: Term Model -> String -> λ* -> HTTP Response
+// dictionaryF --------------------------------------------------------------------------
+// this higher order function generates the handler for the update lambda
+// when specialized with term, CHALLENGE_TOKEN, and bot.
+// this is done to allow dependency injection and, in general
+// to ease testing.
+//
+// dictionaryF :: Model Term -> String -> Slack Bot -> AWS Event -> HTTP Response
 const dictionaryF = (term, CHALLENGE_TOKEN, bot) => async (event) => {
-    console.log("triggering event", event);
     const body = JSON.parse(R.path(["body"], event));
 
     if( body && "challenge" in body && R.path(["token"], body) === CHALLENGE_TOKEN ){
@@ -34,7 +40,7 @@ const dictionaryF = (term, CHALLENGE_TOKEN, bot) => async (event) => {
             statusCode: 200,
             body: JSON.stringify({
                 challenge: R.path(["challenge"], body)
-            }),
+            })
         };
     }
 
@@ -50,7 +56,7 @@ const dictionaryF = (term, CHALLENGE_TOKEN, bot) => async (event) => {
         };
     }
     
-    const { channel, bot_id, text } = R.path(["event"], body);
+    const { channel, bot_id, text } = R.defaultTo({}, R.path(["event"], body));
     
     // ingore messages form other bots (and myself)
     if ( bot_id ) return "200 OK";
@@ -64,7 +70,10 @@ const dictionaryF = (term, CHALLENGE_TOKEN, bot) => async (event) => {
     const messageSent = await bot.chat.postMessage({
         token: TOKEN,
         channel,
-        text: R.defaultTo("Sorry, I didn't get that...", R.path(["Item", "Description", "S"], reply))
+        text: R.defaultTo(
+            "Sorry, I didn't get that...",
+            R.path(["Item", "Description", "S"], reply)
+        )
     });
 
     return {
@@ -77,23 +86,35 @@ const dictionaryF = (term, CHALLENGE_TOKEN, bot) => async (event) => {
 
 };
 
-// updateF :: λ -> Bucket Model -> λ -> λ* -> Promise
-const updateF = (actions, bucket, series) => 
-    async (event) => 
-        Promise                                                             // Data Flow
-            .resolve( bucket.itemsFromEvent(event) )                        // ['key', 'key', ... ]
-            .then( keys => Promise.all(R.map(bucket.download, keys)))       // [Buffer, Buffer, ...]
-            .then( R.map(bucket.decode) )                                   // [[def, def], [def], ...]
-            .then( R.flatten )                                              // [def, def, def, ...]
-            .then( R.map(actions) )                                         // [λ, λ, ... ]
-            .then(series);                                                   // executes side effects                                   
-
 // exports for testing purposes 
 module.exports.testUpdateF = updateF;
 module.exports.testDictionaryF = dictionaryF;
 
+// updateF :: ---------------------------------------------------------------------------
+// this higher order function generates the handler for the update lambda
+// when specialized with actionMapper, bucket and series
+// this is done to allow dependency injection and, in general
+// to ease testing.
+//
+// updateF :: Function -> Model Bucket -> Function -> AWS Event -> Promise
+const updateF = (actionMapper, bucket, series) => 
+    async (event) => 
+        Promise                                                             // Data Flow :
+            .resolve( bucket.itemsFromEvent(event) )                        // ['key', 'key', ... ]
+            .then( keys => Promise.all(R.map(bucket.download, keys)))       // [Buffer, Buffer, ...]
+            .then( R.map(bucket.decode) )                                   // [[def, def], [def], ...]
+            .then( R.flatten )                                              // [def, def, def, ...]
+            .then( R.map(actionMapper) )                                    // [λ, λ, ... ]
+            .then(series);                                                  // executes side effects                                   
+
 // update :: AWS Event -> Promise
+// this is the handler for the update lambda
+// it's a specialized version of updateF
 module.exports.update = updateF(term.actionsFromDefinitions(term), bucket, series);
 
+// dictionary ---------------------------------------------------------------------------
+// this is the handler for the dictionary lambda
+// it's a specialized version of dictionaryF
+//
 // dictionary :: AWS Event -> Promise
 module.exports.dictionary = dictionaryF(term, CHALLENGE_TOKEN, bot);
